074ec7ecac53eb74999538319e5efb56b52b13bc
Trey Hunner <treyhunner@gmail.com>
2012-10-29 22:30:49 -0700

    Add zip function
    This implementation might be too clever.  I wrote it as compact as
    possible first because it's easy to make the code more verbose.
    
    Questions @alaindomissy:
    
    1. Should we remove the use of the ``X if Y else Z`` statement?
    2. Should we replace the list comprehension(s) with for loops?

--- functional_programming/functions.py
+++ functional_programming/functions.py
@@ -4,3 +4,11 @@ def partial(func, *args, **kwargs):
         return func(*(args + more_args),
                     **dict(kwargs.items() + more_kwargs.items()))
     return wrapper
+
+
+def zip(*iterables):
+    results = []
+    shortest_length = min(len(i) for i in iterables) if iterables else 0
+    for n in range(shortest_length):
+        results.append(tuple([iterable[n] for iterable in iterables]))
+    return results
